cb45f844f006410406b3f4e25ae75a0b22cf1fe2
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-20 19:13:18 +0100

    loadout and user models

---
+++ lib/database/models/loadout.model.ts
@@ -0,0 +1,46 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+    _id: string;
+    title: string;
+    weapon: string;
+    gameMode?: string;
+    description?: string;
+    firstAttachment: string
+    secondAttachment: string
+    thirdAttachment: string
+    fourthAttachment: string
+    fifthAttachment: string
+    // firstPerk?: string,
+    // secondPerk?: string,
+    // thirdPerk?: string,
+    createdAt: Date;
+    imageUrl: string;
+    url?: string;
+    category: { _id: string, name: string }
+    creator: { _id: string, firstName: string, lastName: string }
+}
+
+const LoadoutSchema = new Schema({
+    title: { type: String, required: true },
+    weapon: { type: String, required: true },
+    gameMode: { type: String },
+    description: { type: String },
+    firstAttachment: { type: String, required: true },
+    secondAttachment: { type: String, required: true },
+    thirdAttachment: { type: String, required: true },
+    fourthAttachment: { type: String, required: true },
+    fifthAttachment: { type: String, required: true },
+    // firstPerk: { type: String },
+    // secondPerk: { type: String },
+    // thirdPerk: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    imageUrl: { type: String, required: true },
+    url: { type: String },
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+    creator: { type: Schema.Types.ObjectId, ref: 'User' },
+})
+
+const Event = models.Event || model('Event', LoadoutSchema);
+
+export default Event;
